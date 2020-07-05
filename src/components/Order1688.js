424b46206a125acc78ee6bbec08aca9841f8b659
jemo <1321332562@qq.com>
2020-07-05 16:45:54 +0800

    1688 order list

--- src/components/Order1688.js
+++ src/components/Order1688.js
@@ -4,19 +4,38 @@
  * 1688 Order
  */
 
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
 import Snackbar from '@material-ui/core/Snackbar';
 import Button from '@material-ui/core/Button';
+import MaterialTable from 'material-table';
+import tableIcons from './utils/TableIcons';
 
 function Order1688() {
 
+  const [ orderList, setOrderList ] = useState([]);
   const [ snackbarState, setSnackbarState ] = useState({
     open: false,
     message: '',
   });
   const { open, message } = snackbarState;
+
+  useEffect(() => {
+    const fetchOrderList = async () => {
+      try {
+        const { data } = await axios.get('/order1688List');
+        setOrderList(data);
+      }
+      catch(err) {
+        console.error('order-1688-fetch-order-list-error: ', err);
+        handleOpenSnackbar({
+          message: `出错了：${err.message}`,
+        });
+      }
+    }
+    fetchOrderList();
+  }, []);
 
   const handleOpenSnackbar = ({ message }) => {
     setSnackbarState({
@@ -44,21 +63,19 @@ function Order1688() {
       handleOpenSnackbar({
         message: '操作成功',
       });
-      /*
       const fetchOrderList = async () => {
         try {
-          const { data } = await axios.get('/orderList');
+          const { data } = await axios.get('/order1688List');
           setOrderList(data);
         }
         catch(err) {
-          console.error('order-fetch-order-list-error: ', err);
+          console.error('order-1688-fetch-order-list-error: ', err);
           handleOpenSnackbar({
             message: `出错了：${err.message}`,
           });
         }
       }
       fetchOrderList();
-      */
     }
     catch(err) {
       handleOpenSnackbar({
@@ -66,7 +83,6 @@ function Order1688() {
       });
       console.error("OrderOnDropError: ", err);
     }
-
   }, []);
 
   const {
@@ -77,7 +93,187 @@ function Order1688() {
 
   return (
     <div>
-      1688 订单
+      <MaterialTable
+        icons={tableIcons}
+        columns={[
+          {
+            title: "订单编号",
+            field: "orderId",
+            cellStyle: {
+              fontSize: 12,
+            },
+          },
+          {
+            title: "商品信息",
+            render: rowData => {
+              const {
+                productTitle,
+                price,
+                amount,
+              } = rowData;
+              return (
+                <div style={{
+                  width: 200,
+                }}>
+                  <div style={{
+                    fontSize: 12,
+                  }}>
+                    {productTitle}
+                  </div>
+                  <div
+                    style={{
+                      fontSize: 12,
+                      color: '#888888',
+                    }}>
+                    单价：￥{price}
+                  </div>
+                  <div
+                    style={{
+                      fontSize: 12,
+                      color: '#888888',
+                    }}>
+                    数量：{amount}
+                  </div>
+                </div>
+              );
+            },
+          },
+          {
+            title: "订单状态",
+            field: "orderStatus",
+            lookup: {
+              '等待买家确认收货': '等待买家确认收货',
+              '已收货': '已收货',
+              '交易关闭': '交易关闭',
+              '交易成功': '交易成功',
+            },
+            cellStyle: {
+              fontSize: 12,
+            },
+          },
+          {
+            title: "实付金额(元)",
+            render: rowData => {
+              const {
+                actualPayment,
+                totalPrice,
+                shippingFare,
+                discount,
+              } = rowData;
+              return (
+                <div>
+                  <div style={{
+                    fontSize: 12,
+                  }}>
+                    ￥{actualPayment}
+                  </div>
+                  <div style={{
+                    fontSize: 12,
+                    color: '#888888',
+                  }}>
+                    <div>
+                      总价: ￥{totalPrice}
+                    </div>
+                    <div>
+                      运费: ￥{shippingFare}
+                    </div>
+                    <div>
+                      折扣: ￥{discount}
+                    </div>
+                  </div>
+                </div>
+              );
+            },
+          },
+          {
+            title: "收货人",
+            field: "receiver",
+            cellStyle: {
+              fontSize: 12,
+            },
+          },
+          {
+            title: "下单时间",
+            render: rowData => {
+              const {
+                orderCreatedTime,
+                orderPaymentTime,
+              } = rowData;
+              return (
+                <div style={{
+                  fontSize: 12,
+                  color: '#888888',
+                  width: 150,
+                }}>
+                  <div>
+                    订单创建时间:
+                  </div>
+                  <div>
+                    {orderCreatedTime}
+                  </div>
+                  <div>
+                    订单支付时间:
+                  </div>
+                  <div>
+                    {orderPaymentTime}
+                  </div>
+                </div>
+              );
+            },
+          },
+          {
+            title: "收货信息",
+            render: rowData => {
+              const {
+                receiver,
+                phone,
+                shippingAddress,
+              } = rowData;
+              return (
+                <div style={{
+                  fontSize: 12,
+                  width: 150,
+                }}>
+                  <div>
+                    收件人: {`${receiver} ${phone}`}
+                  </div>
+                  <div>
+                    联系地址: {shippingAddress}
+                  </div>
+                </div>
+              );
+            },
+          },
+          {
+            title: "物流信息",
+            render: rowData => {
+              const {
+                courierCompany,
+                trackingNumber,
+              } = rowData;
+              return (
+                <div
+                  style={{
+                    fontSize: 12,
+                    width: 150,
+                  }}>
+                  <div>
+                    快递公司: {courierCompany}
+                  </div>
+                  <div>
+                    快递单号: {trackingNumber}
+                  </div>
+                </div>
+              );
+            },
+          },
+        ]}
+        data={orderList}
+        title="订单列表"
+        options={{
+          filtering: true,
+        }}
+      />
       <div {...getRootProps()}>
         <input {...getInputProps()} />
         {
