cb1d563b9aecaa62805162a652ba78e2efc88c47
jemo <1321332562@qq.com>
2021-03-08 20:11:08 +0800

    download limit time

--- src/components/AdData.js
+++ src/components/AdData.js
@@ -22,21 +22,31 @@ function AdData () {
   const [ snackbarState, setSnackbarState ] = useState({
     message: '',
     open: false,
+    autoHideDuration: null,
   });
   const [ adPlanList, setAdPlanList ] = useState([]);
   const [ scenesType, setScenesType ] = useState(0);
-  const { message, open } = snackbarState;
+  const { message, open, autoHideDuration } = snackbarState;
 
   const handleOpenSnackbar = ({message}) => {
     setSnackbarState({
       message,
       open: true,
+      autoHideDuration: 2000,
     });
   }
 
   const handleCloseSnackbar = () => {
     setSnackbarState({
       open: false,
+    });
+  }
+
+  const handleOpenErrorSnackbar = ({ message }) => {
+    setSnackbarState({
+      message,
+      open: true,
+      autoHideDuration: null,
     });
   }
 
@@ -98,75 +108,116 @@ function AdData () {
     setScenesType(event.target.value);
   }
 
+  const [ columns ] = useState([
+    {
+      title: '商家Id',
+      field: 'mallId',
+      cellStyle: {
+        fontSize: 12,
+      },
+      filtering: false,
+    },
+    {
+      title: '计划Id',
+      field: 'planId',
+      cellStyle: {
+        fontSize: 12,
+      },
+      filtering: false,
+    },
+    {
+      title: '计划名称',
+      field: 'planName',
+      cellStyle: {
+        fontSize: 12,
+      },
+      render: rowData => {
+        const {
+          planId,
+          planName,
+        } = rowData;
+        return (
+          <Link to={`/adPlan/${planId}`}>
+            {planName}
+          </Link>
+        );
+      },
+    },
+    {
+      title: '下载限时折扣',
+      field: 'downloadLimitTime',
+      cellStyle: {
+        fontSize: 12,
+      },
+      render: rowData => {
+        const {
+          planId,
+        } = rowData;
+        return (
+          <div>
+            <Button
+              variant='outlined'
+              color='primary'
+              onClick={async () => {
+                try {
+                  await axios.get('/downloadLimitTime', {
+                    params: {
+                      planId: planId,
+                    },
+                  });
+                  handleOpenSnackbar({
+                    message: '下载成功',
+                  });
+                }
+                catch(err) {
+                  console.error('AdData.js-download-limit-time-catch-error: ', err)
+                  handleOpenErrorSnackbar({
+                    message: `出错了：${err.response && err.response.data}`,
+                  });
+                }
+              }}>
+              下载限时折扣表格
+            </Button>
+          </div>
+        );
+      },
+    },
+    {
+      title: 'isStick',
+      field: 'isStick',
+      type: 'boolean',
+      cellStyle: {
+        fontSize: 12,
+      },
+      filtering: false,
+    },
+    {
+      title: 'stickTime',
+      field: 'stickTime',
+      cellStyle: {
+        fontSize: 12,
+      },
+      filtering: false,
+    },
+    {
+      title: '类型',
+      field: 'scenesType',
+      lookup: {
+        0: '多多搜索',
+        1: '聚焦展位',
+        2: '多多场景',
+      },
+      cellStyle: {
+        fontSize: 12,
+      },
+    },
+  ]);
+
   return (
     <div>
       <MaterialTable
         icons={tableIcons}
-        columns={[
-          {
-            title: '商家Id',
-            field: 'mallId',
-            cellStyle: {
-              fontSize: 12,
-            },
-            filtering: false,
-          },
-          {
-            title: '计划Id',
-            field: 'planId',
-            cellStyle: {
-              fontSize: 12,
-            },
-            filtering: false,
-          },
-          {
-            title: '计划名称',
-            field: 'planName',
-            cellStyle: {
-              fontSize: 12,
-            },
-            render: rowData => {
-              const {
-                planId,
-                planName,
-              } = rowData;
-              return (
-                <Link to={`/adPlan/${planId}`}>
-                  {planName}
-                </Link>
-              );
-            },
-          },
-          {
-            title: 'isStick',
-            field: 'isStick',
-            type: 'boolean',
-            cellStyle: {
-              fontSize: 12,
-            },
-            filtering: false,
-          },
-          {
-            title: 'stickTime',
-            field: 'stickTime',
-            cellStyle: {
-              fontSize: 12,
-            },
-            filtering: false,
-          },
-          {
-            title: '类型',
-            field: 'scenesType',
-            lookup: {
-              0: '多多搜索',
-              1: '聚焦展位',
-              2: '多多场景',
-            },
-            cellStyle: {
-              fontSize: 12,
-            },
-          },
-        ]}
+        columns={columns}
         data={adPlanList}
         title="推广计划列表"
         options={{
@@ -215,7 +266,7 @@ function AdData () {
           horizontal: "center",
           vertical: "top",
         }}
-        autoHideDuration={2000}
+        autoHideDuration={autoHideDuration}
         open={open}
         onClose={handleCloseSnackbar}
         message={message}
